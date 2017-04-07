78caf43f1a6de636aa18138ec2c8faf26f5efbce
Alexander Huleatt <alexhuleatt@Alexanders-MacBook-Pro.local>
2017-04-07 13:07:45 -0600

    added more files

--- db_dev.py
+++ db_dev.py
@@ -1,2 +1,15 @@
-import tkinter
-print "hi"
+import MySQLdb
+
+
+db = MySQLdb.connect("localhost", "root", "4ebb9q1", "nppes_1")
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SHOW TABLES")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchall()
+
+print data
--- web_parser.py
+++ web_parser.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
-import urllib, re, zipfile, os, os.path, shutil
+import urllib
+import re
 import zipfile
+import os
+from os.path import join
+import shutil
 
 '''
 Change this if the download page ever changed.
@@ -12,46 +16,61 @@ link_page = download_page + 'NPI_Files.html'
 This would be prone to change if the website ever changed.
 This would require modification to continue to function.
 '''
-zip_regex = './(NPPES_Data_Dissemination_' + '.*' + '_Weekly.zip)'
+zip_regex = join('.', '(NPPES_Data_Dissemination_' + '.*' + '_Weekly.zip)')
+
 
 def get_download_links():
-	r = urllib.urlopen(link_page).read()
-	soup = BeautifulSoup(r, 'html.parser')
-	links = soup.find_all('a', href=True)
+    '''This returns all download links for weekly update files.'''
+    r = urllib.urlopen(link_page).read()
+    soup = BeautifulSoup(r, 'html.parser')
+    links = soup.find_all('a', href=True)
 
-	
-	pattern = re.compile(zip_regex)
+    pattern = re.compile(zip_regex)
 
-	weekly_links = []
-	for l in links:
-		path = l['href']
-		m = pattern.match(path)
-		if m:
-			weekly_links.append(download_page + m.groups()[0])
+    weekly_links = []
+    for l in links:
+        path = l['href']
+        m = pattern.match(path)
+        if m:
+            weekly_links.append(download_page + m.groups()[0])
 
-	return weekly_links
-
+    return weekly_links
 
 
-csv_regex = 'npidata_[\\d_]*\\.csv'
+'''
+this regex is mainly used to avoid getting the header csv file
+Put out here in case of future changes.
+
+Should match *only* the update csv file.
+'''
+csv_regex = 'npidata_[\\d_\-]*\\.csv'
+
+
 def retrieve_csv_file(url):
-	urllib.urlretrieve(url, 'week.zip')
-	zip_ref = zipfile.ZipFile('week.zip', 'r')
-	zip_ref.extractall('./week/')
-	zip_ref.close()
-	os.remove('week.zip')
-	p = re.compile(csv_regex)
-	fname = ''
-	for _,_,files in os.walk('./week/'):
-		for f in files:
-			if p.match(f):
-				os.rename('./'+f, f)
-				fname = f
-	shutil.rmtree('./week/')
-	return fname
+    '''
+    This function downloads, upzips, and grabs the desired csv file.
+    Theile is placed at the working directory and the filename is returned.
+    All extraneous files/folders are deleted
+    '''
+    week_dir = join('.', 'week')
+    urllib.urlretrieve(url, 'week.zip')
+    zip_ref = zipfile.ZipFile('week.zip', 'r')
+    zip_ref.extractall(week_dir)
+    zip_ref.close()
+    os.remove('week.zip')
+    p = re.compile(csv_regex)
+    fname = ''
+    for _, _, files in os.walk(week_dir):
+        for f in files:
+            if p.match(f):
+                from_dir = join(week_dir, f)
+                to_dir = join('.', f)
+                os.rename(from_dir, to_dir)
+                fname = f
+    shutil.rmtree(week_dir)
+    return fname
 
 
-
-
-links = get_download_links()
-retrieve_csv_file(links[0])
+if __name__ == '__main__':
+    links = get_download_links()
+    retrieve_csv_file(links[0])
