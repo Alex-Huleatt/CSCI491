2248a6699791431665f268914068e65de098761c
Alexander Huleatt <alexhuleatt@Alexanders-MacBook-Pro.local>
2017-04-18 08:42:39 -0600

    improvements to runner

--- runner.py
+++ runner.py
@@ -5,9 +5,9 @@ import web_parser, csvParser
 def main():
     dl_links = web_parser.get_download_links()
     csv_name = web_parser.retrieve_csv_file(dl_links[0])
-    gener = csvParser.get_generator(csv_name)
-    print 'Got generator for %s from %s: ' % (csv_name, dl_links[0]), gener
-
+    up = csvParser.CSV(csv_name)
+    headers = csvParser.loadHeaders('header.csv')
+    print {'headers':headers, 'CSV':up, 'link':dl_links[0]}
 if __name__ == '__main__':
     main()
 
