5b90422657451aa8153aa622ca3a07267005964a
Kevin Ripley <kevin.r.ripley.kr@gmail.com>
2017-04-21 15:38:22 -0600

    update comments on cli

--- cli.py
+++ cli.py
@@ -1,16 +1,21 @@
 import argparse
+import sys
 
 
 parser = argparse.ArgumentParser(
     description='NPPES database and file handler script.')
-
+#Add Option for full reset
 parser.add_argument(
     '--fullreset', help='Not supported.', action='store_true', default=False)
-
+#Option for Verbose mode
 parser.add_argument('--v', help='Verbose mode',
                     action='store_true', default=False)
+#Add Option for Days till Update
 parser.add_argument('--update', type=float, help='Set days till update.', default=7.0)
-
+#Add Option to Run Deactivation File
 parser.add_argument('--deactivate', help='Run Deactivation File.')
-
-print parser.parse_args()
+#Setting args to the arguments to Run
+args = parser.parse_args()
+#write the file on stdout
+#ex: write(get_download_links) from web_parser.py
+#sys.stdout.write()
